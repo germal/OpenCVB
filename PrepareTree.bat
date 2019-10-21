62b6956b8b4c39ba0f884f126868f86a93ab5a80
Bob Davies <bobdavies2000@gmail.com>
2019-10-21 15:27:01 -0700

    librealsense cmake configuration can't use OpenCV directory yet because it hasn't been built.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -7,5 +7,5 @@ cd ..\
 
 
 "C:\Program Files\CMake\bin\Cmake.exe" -DWith_OPENGL=1 -DBUILD_PERF_TESTS=0 -DBUILD_TESTS=0 -DBUILD_opencv_python_tests=0 -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S OpenCV -B OpenCV/Build
-"C:\Program Files\CMake\bin\Cmake.exe" -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=1 -DOpenCV_DIR=OpenCV/Build -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S librealsense -B librealsense/Build
+"C:\Program Files\CMake\bin\Cmake.exe" -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=0 -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S librealsense -B librealsense/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DOpenCV_DIR=OpenCV/Build -DCMAKE_BUILD_TYPE=Debug -S Azure-Kinect-Sensor-SDK -B Azure-Kinect-Sensor-SDK/Build
