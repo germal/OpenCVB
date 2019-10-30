de13526a65c19128e57ec2bcb94a9158b8d0f9ed
Bob Davies <bobdavies2000@gmail.com>
2019-10-30 09:58:36 -0700

    Got the OpenCV OpenGL interface configured properly in PrepareTree.bat.

--- VB_Classes/OpenCVGL.vb
+++ VB_Classes/OpenCVGL.vb
@@ -20,66 +20,65 @@ End Module
 
 
 ' https://github.com/opencv/opencv/blob/master/samples/cpp/detect_mser.cpp
-'Public Class OpenCVGL_Image_CPP : Implements IDisposable
-'    Dim cloud As Depth_XYZ_OpenMP_CPP
-'    Dim imu As IMU_Basics
-'    Dim rgbData() As Byte
-'    Dim pointCloudData() As Byte
-'    Public sliders As New OptionsSliders
-'    Public sliders1 As New OptionsSliders
-'    Public sliders2 As New OptionsSliders
-'    Public sliders3 As New OptionsSliders
-'    Public Sub New(ocvb As AlgorithmData)
-'        If ocvb.parms.UsingIntelCamera Then cloud = New Depth_XYZ_OpenMP_CPP(ocvb)
+Public Class OpenCVGL_Image_CPP : Implements IDisposable
+    Dim cloud As Depth_XYZ_OpenMP_CPP
+    Dim imu As IMU_Basics
+    Dim rgbData() As Byte
+    Dim pointCloudData() As Byte
+    Public sliders As New OptionsSliders
+    Public sliders1 As New OptionsSliders
+    Public sliders2 As New OptionsSliders
+    Public sliders3 As New OptionsSliders
+    Public Sub New(ocvb As AlgorithmData)
+        If ocvb.parms.UsingIntelCamera Then cloud = New Depth_XYZ_OpenMP_CPP(ocvb)
 
-'        imu = New IMU_Basics(ocvb)
-'        imu.externalUse = True
+        imu = New IMU_Basics(ocvb)
+        imu.externalUse = True
 
-'        setOpenGLsliders(ocvb, sliders, sliders1, sliders2, sliders3)
-'        sliders2.TrackBar3.Value = -10 ' eye.z
-'        sliders.TrackBar1.Value = 8 ' FOV
-'        sliders.TrackBar2.Value = 0 ' Yaw
-'        sliders.TrackBar3.Value = 0 ' pitch
-'        sliders.TrackBar4.Value = 0 ' roll
-'        If ocvb.parms.UsingIntelCamera = False Then sliders.TrackBar1.Value = 35
+        setOpenGLsliders(ocvb, sliders, sliders1, sliders2, sliders3)
+        sliders2.TrackBar3.Value = -10 ' eye.z
+        sliders.TrackBar1.Value = 8 ' FOV
+        sliders.TrackBar2.Value = 0 ' Yaw
+        sliders.TrackBar3.Value = 0 ' pitch
+        sliders.TrackBar4.Value = 0 ' roll
+        If ocvb.parms.UsingIntelCamera = False Then sliders.TrackBar1.Value = 35
 
+        ReDim rgbData(ocvb.color.Total * ocvb.color.ElemSize - 1)
 
-'        ReDim rgbData(ocvb.color.Total * ocvb.color.ElemSize - 1)
+        OpenCVGL_Image_Open(ocvb.color.Width, ocvb.color.Height)
+        ocvb.desc = "Use the OpenCV implementation of OpenGL to render a 3D image with depth."
+    End Sub
+    Public Sub Run(ocvb As AlgorithmData)
+        If ocvb.frameCount = 0 Then ReDim pointCloudData(ocvb.parms.pcBufferSize - 1)
 
-'        OpenCVGL_Image_Open(1280, 720) ' ocvb.openGLWidth, ocvb.openGLHeight)
-'        ocvb.desc = "Use the OpenCV implementation of OpenGL to render a 3D image with depth."
-'    End Sub
-'    Public Sub Run(ocvb As AlgorithmData)
-'        If ocvb.frameCount = 0 Then ReDim pointCloudData(ocvb.parms.pcBufferSize - 1)
+        imu.Run(ocvb)
+        Dim FOV = sliders.TrackBar1.Value
+        Dim yaw = sliders.TrackBar2.Value
+        Dim pitch = sliders.TrackBar3.Value
+        Dim roll = sliders.TrackBar4.Value
+        Dim zNear = sliders1.TrackBar1.Value
+        Dim zFar = sliders1.TrackBar2.Value
+        Dim pointSize = sliders1.TrackBar3.Value
+        Dim eye As New cv.Vec3f(sliders2.TrackBar1.Value, sliders2.TrackBar2.Value, sliders2.TrackBar3.Value)
+        Dim zTrans = sliders1.TrackBar4.Value / 100
 
-'        imu.Run(ocvb)
-'        Dim FOV = sliders.TrackBar1.Value
-'        Dim yaw = sliders.TrackBar2.Value
-'        Dim pitch = sliders.TrackBar3.Value
-'        Dim roll = sliders.TrackBar4.Value
-'        Dim zNear = sliders1.TrackBar1.Value
-'        Dim zFar = sliders1.TrackBar2.Value
-'        Dim pointSize = sliders1.TrackBar3.Value
-'        Dim eye As New cv.Vec3f(sliders2.TrackBar1.Value, sliders2.TrackBar2.Value, sliders2.TrackBar3.Value)
-'        Dim zTrans = sliders1.TrackBar4.Value / 100
+        OpenCVGL_Image_Control(ocvb.parms.intrinsics.ppx, ocvb.parms.intrinsics.ppy, ocvb.parms.intrinsics.fx, ocvb.parms.intrinsics.fy, FOV, zNear, zFar, eye, yaw, roll, pitch, pointSize, zTrans,
+                                       ocvb.color.Width, ocvb.color.Height)
 
-'        OpenCVGL_Image_Control(ocvb.parms.intrinsics.ppx, ocvb.parms.intrinsics.ppy, ocvb.parms.intrinsics.fx, ocvb.parms.intrinsics.fy, FOV, zNear, zFar, eye, yaw, roll, pitch, pointSize, zTrans,
-'                                       ocvb.color.Width, ocvb.color.Height)
-
-'        Marshal.Copy(ocvb.color.Data, rgbData, 0, rgbData.Length)
-'        Marshal.Copy(ocvb.pointCloud.Data, pointCloudData, 0, ocvb.parms.pcBufferSize)
-'        Dim handleRGB = GCHandle.Alloc(rgbData, GCHandleType.Pinned)
-'        Dim handlePointCloud = GCHandle.Alloc(pointCloudData, GCHandleType.Pinned)
-'        OpenCVGL_Image_Run(handleRGB.AddrOfPinnedObject(), handlePointCloud.AddrOfPinnedObject(), ocvb.parms.pcBufferSize, ocvb.color.Rows, ocvb.color.Cols)
-'        handleRGB.Free()
-'        handlePointCloud.Free()
-'    End Sub
-'    Public Sub Dispose() Implements IDisposable.Dispose
-'        sliders.Dispose()
-'        sliders1.Dispose()
-'        sliders2.Dispose()
-'        sliders3.Dispose()
-'        OpenCVGL_Image_Close()
-'        imu.Dispose()
-'    End Sub
-'End Class
+        Marshal.Copy(ocvb.color.Data, rgbData, 0, rgbData.Length)
+        Marshal.Copy(ocvb.pointCloud.Data, pointCloudData, 0, ocvb.parms.pcBufferSize)
+        Dim handleRGB = GCHandle.Alloc(rgbData, GCHandleType.Pinned)
+        Dim handlePointCloud = GCHandle.Alloc(pointCloudData, GCHandleType.Pinned)
+        OpenCVGL_Image_Run(handleRGB.AddrOfPinnedObject(), handlePointCloud.AddrOfPinnedObject(), ocvb.parms.pcBufferSize, ocvb.color.Rows, ocvb.color.Cols)
+        handleRGB.Free()
+        handlePointCloud.Free()
+    End Sub
+    Public Sub Dispose() Implements IDisposable.Dispose
+        sliders.Dispose()
+        sliders1.Dispose()
+        sliders2.Dispose()
+        sliders3.Dispose()
+        OpenCVGL_Image_Close()
+        imu.Dispose()
+    End Sub
+End Class
