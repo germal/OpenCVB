fd7674ff8c4152181f9909ef10c54ad73902a0d0
Bob Davies <bobdavies2000@gmail.com>
2019-11-17 13:17:17 -0800

    Prettified the options a little to make it clear only Debug and Release are desired.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -13,8 +13,8 @@ cd OpenCV
 "c:\Program Files\Git\bin\git.exe" clone "https://github.com/opencv/opencv_contrib"
 cd ..\
 
-"C:\Program Files\CMake\bin\Cmake.exe" -DWITH_OPENGL=ON -DWITH_VTK=ON -DBUILD_EXAMPLES=ON -DCPU_DISPATCH=SSE4_1;SSE4_2;AVX;FP16 -DVTK_DIR=VTK/Build/ -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_python_tests=OFF -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S OpenCV -B OpenCV/Build
-"C:\Program Files\CMake\bin\Cmake.exe" -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=0 -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S librealsense -B librealsense/Build
+"C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DWITH_OPENGL=ON -DWITH_VTK=ON -DBUILD_EXAMPLES=ON -DCPU_DISPATCH=SSE4_1;SSE4_2;AVX;FP16 -DVTK_DIR=VTK/Build/ -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_python_tests=OFF -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -S OpenCV -B OpenCV/Build
+"C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=0 -S librealsense -B librealsense/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DOpenCV_DIR=OpenCV/Build -DCMAKE_BUILD_TYPE=Debug -S Azure-Kinect-Sensor-SDK -B Azure-Kinect-Sensor-SDK/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S eigen-git-mirror -B eigen-git-mirror/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DVTK_BUILD_EXAMPLES=1 -S VTK -B VTK/Build
