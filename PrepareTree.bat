5ae9e4106a72a5aedd1eca9107a612ce1588a971
Bob Davies <bobdavies2000@gmail.com>
2019-11-18 14:23:07 -0800

    Removed the examples from the OpenCV build because it takes too long.  Added generic indexer to pixel manipulation example.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -13,7 +13,7 @@ cd OpenCV
 "c:\Program Files\Git\bin\git.exe" clone "https://github.com/opencv/opencv_contrib"
 cd ..\
 
-"C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DWITH_OPENGL=ON -DWITH_VTK=ON -DBUILD_EXAMPLES=ON -DCPU_DISPATCH=SSE4_1;SSE4_2;AVX;FP16 -DVTK_DIR=VTK/Build/ -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_python_tests=OFF -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -S OpenCV -B OpenCV/Build
+"C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DWITH_OPENGL=ON -DWITH_VTK=ON -DBUILD_EXAMPLES=OFF -DCPU_DISPATCH=SSE4_1;SSE4_2;AVX;FP16 -DVTK_DIR=VTK/Build/ -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_python_tests=OFF -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -S OpenCV -B OpenCV/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=0 -S librealsense -B librealsense/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DOpenCV_DIR=OpenCV/Build -DCMAKE_BUILD_TYPE=Debug -S Azure-Kinect-Sensor-SDK -B Azure-Kinect-Sensor-SDK/Build
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -S eigen-git-mirror -B eigen-git-mirror/Build
@@ -25,5 +25,5 @@ start librealsense/Build/librealsense2.sln
 start opencvsharp/OpenCvSharp.sln
 start opencvsharp/samples/OpenCvSharpSamples.sln
 start eigen-git-mirror/Build/eigen3.sln
-start VTK/Build/vtk.sln
-start OpenCV/Build/OpenCV.sln
+rem start VTK/Build/vtk.sln
+rem start OpenCV/Build/OpenCV.sln
--- VB_Classes/Pixel.vb
+++ VB_Classes/Pixel.vb
@@ -10,6 +10,7 @@ Public Class Pixel_GetSet : Implements IDisposable
         ocvb.desc = "Perform Pixel-level operations in 3 different ways to measure efficiency."
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
+        Dim watch = Stopwatch.StartNew()
         For y = 0 To ocvb.color.Height - 1
             For x = 0 To ocvb.color.Width - 1
                 Dim color = ocvb.color.Get(Of cv.Vec3b)(y, x)
@@ -17,6 +18,25 @@ Public Class Pixel_GetSet : Implements IDisposable
                 ocvb.result1.Set(Of cv.Vec3b)(y, x, color)
             Next
         Next
+        watch.Stop()
+        Console.WriteLine("GetSet took " + CStr(watch.ElapsedMilliseconds) + "ms")
+
+        ocvb.result2 = ocvb.color.Clone()
+        watch = Stopwatch.StartNew()
+        Dim indexer = ocvb.result2.GetGenericIndexer(Of cv.Vec3b)
+        For y = 0 To ocvb.result2.Height - 1
+            For x = 0 To ocvb.result2.Width - 1
+                Dim color = indexer(y, x)
+                Swap(color.Item0, color.Item2)
+                indexer(y, x) = color
+            Next
+        Next
+        watch.Stop()
+        Console.WriteLine("Indexer took " + CStr(watch.ElapsedMilliseconds) + "ms")
+
+        'Dim mat = New cv.Mat(Of Byte)(ocvb.color.Data)
+        'indexer = mat.GetIndexer()
+
     End Sub
     Public Sub Dispose() Implements IDisposable.Dispose
     End Sub
