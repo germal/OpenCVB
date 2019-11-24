7c079c6942533dcef36d7fce58af06e1d6856166
Bob Davies <bobdavies2000@gmail.com>
2019-11-24 10:56:21 -0800

    Added an experiment to use the OpenCV TRACE facility.

---
+++ CPP_Classes/Trace.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/utils/trace.hpp>
+
+using namespace std;
+using namespace cv;
+class Trace_OpenCV
+{
+private:
+public:
+	Mat gray, processed;
+	Mat src, dst;
+    Trace_OpenCV(){}
+    void Run() {
+		cv::cvtColor(src, gray, COLOR_BGR2GRAY);
+		Canny(gray, processed, 32, 64, 3);
+	}
+};
+
+extern "C" __declspec(dllexport)
+Trace_OpenCV *Trace_OpenCV_Open() {
+    Trace_OpenCV *Trace_OpenCVPtr = new Trace_OpenCV();
+	CV_TRACE_REGION("Start");
+	return Trace_OpenCVPtr;
+}
+
+extern "C" __declspec(dllexport)
+void Trace_OpenCV_Close(Trace_OpenCV *Trace_OpenCVPtr)
+{
+    delete Trace_OpenCVPtr;
+}
+
+extern "C" __declspec(dllexport)
+int *Trace_OpenCV_Run(Trace_OpenCV *Trace_OpenCVPtr, int *rgbPtr, int rows, int cols, int channels)
+{
+	CV_TRACE_REGION_NEXT("process");
+	Trace_OpenCVPtr->src = Mat(rows, cols, (channels == 3) ? CV_8UC3 : CV_8UC1, rgbPtr);
+	Trace_OpenCVPtr->Run();
+
+	CV_TRACE_REGION("read"); // we are off to read the next frame...
+	return (int *) Trace_OpenCVPtr->processed.data; // return this C++ allocated data to managed code
+}
---
+++ VB_Classes/Trace.vb
@@ -0,0 +1,40 @@
+﻿Imports cv = OpenCvSharp
+Imports System.Runtime.InteropServices
+Module Trace_OpenCV_CPP_Module
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Function Trace_OpenCV_Open() As IntPtr
+    End Function
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Sub Trace_OpenCV_Close(Trace_OpenCVPtr As IntPtr)
+    End Sub
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Function Trace_OpenCV_Run(Trace_OpenCVPtr As IntPtr, rgbPtr As IntPtr, rows As Int32, cols As Int32, channels As Int32) As IntPtr
+    End Function
+End Module
+
+Public Class Trace_OpenCV_CPP : Implements IDisposable
+    Dim Trace_OpenCV As IntPtr
+    Public Sub New(ocvb As AlgorithmData)
+        Trace_OpenCV = Trace_OpenCV_Open()
+        ocvb.desc = "Use OpenCV's Trace facility - applicable to C++ code."
+    End Sub
+    Public Sub Run(ocvb As AlgorithmData)
+        Dim src = ocvb.color
+        Dim srcData(src.Total * src.ElemSize) As Byte
+        Marshal.Copy(src.Data, srcData, 0, srcData.Length - 1)
+        Dim handleSrc = GCHandle.Alloc(srcData, GCHandleType.Pinned)
+        Dim imagePtr = Trace_OpenCV_Run(Trace_OpenCV, handleSrc.AddrOfPinnedObject(), src.Rows, src.Cols, src.Channels)
+        handleSrc.Free()
+
+        If imagePtr <> 0 Then
+            Dim dstData(src.Total - 1) As Byte
+            Marshal.Copy(imagePtr, dstData, 0, dstData.Length)
+            ocvb.result1 = New cv.Mat(src.Rows, src.Cols, cv.MatType.CV_8UC1, dstData)
+        End If
+    End Sub
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Trace_OpenCV_Close(Trace_OpenCV)
+    End Sub
+End Class
+
+
