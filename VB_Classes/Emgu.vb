7ea71e24e1fffc2299f294caca6bb8b9168362f1
Bob Davies <bobdavies2000@gmail.com>
2019-12-02 16:51:43 -0800

    Addding support for EMGU applications.

---
+++ VB_Classes/Emgu.vb
@@ -0,0 +1,12 @@
+﻿Imports cv = OpenCvSharp
+
+Public Class Emgu_Basics : Implements IDisposable
+    Public Sub New(ocvb As AlgorithmData)
+        ocvb.desc = "Test a sample EMGU usage."
+    End Sub
+    Public Sub Run(ocvb As AlgorithmData)
+        Dim img = Emgu_Classes.DrawSubdivision.Draw(ocvb.color.Rows, ocvb.color.Cols, 20)
+    End Sub
+    Public Sub Dispose() Implements IDisposable.Dispose
+    End Sub
+End Class
