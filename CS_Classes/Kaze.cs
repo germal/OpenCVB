8965be963fa9b0119ae9b8735ae07c44f8585f78
Bob Davies <bobdavies2000@gmail.com>
2019-12-23 09:02:58 -0800

    Improved the test to prevent divide by zero...

--- CS_Classes/Kaze.cs
+++ CS_Classes/Kaze.cs
@@ -257,15 +257,18 @@ namespace CS_Classes
                 mask.CopyTo(m);
                 for (int i = 0; i < matches.Length; i++)
                 {
-                    if (matches[i][1].Distance > 0)
+                    if (matches[i].Length > 1)
                     {
-                        //This is also known as NNDR Nearest Neighbor Distance Ratio
-                        if ((matches[i][0].Distance / matches[i][1].Distance) <= uniqnessThreshold)
-                            maskData[i] = 255;
-                        else
-                            maskData[i] = 0;
+                        if (matches[i][1].Distance > 0)
+                        {
+                            //This is also known as NNDR Nearest Neighbor Distance Ratio
+                            if ((matches[i][0].Distance / matches[i][1].Distance) <= uniqnessThreshold)
+                                maskData[i] = 255;
+                            else
+                                maskData[i] = 0;
+                        }
                     }
-                }
+                 }
                 m.CopyTo(mask);
             }
             maskHandle.Free();
