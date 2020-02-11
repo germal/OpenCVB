70d859d6633876ec5574fc6d8b5d926192b4e6de
Bob Davies <bobdavies2000@gmail.com>
2020-02-11 15:53:43 -0800

    Added labels for remap and remap flip.  Fixed an old oversight...

--- VB_Classes/Remap.vb
+++ VB_Classes/Remap.vb
@@ -8,19 +8,15 @@ Public Class Remap_Basics : Implements IDisposable
         Dim map_y = New cv.Mat(ocvb.color.Size(), cv.MatType.CV_32F)
         Static Dim direction = 0
 
+        ocvb.label1 = Choose(direction + 1, "Remap_Basics - original", "Remap veritically", "Remap horizontally",
+                                            "Remap horizontally and vertically")
+
         Dim src As cv.Mat = ocvb.color
         ' build a map for use with remap!
         For j = 0 To map_x.Rows - 1
             For i = 0 To map_x.Cols - 1
                 Select Case direction
-                    Case 0
-                        If i > src.Cols * 0.25 And i < src.Cols * 0.75 And j > src.Rows * 0.25 And j < src.Rows * 0.75 Then
-                            map_x.Set(Of Single)(j, i, 2 * (i - src.Cols * 0.25) + 0.5)
-                            map_y.Set(Of Single)(j, i, 2 * (j - src.Rows * 0.25) + 0.5)
-                        Else
-                            map_x.Set(Of Single)(j, i, 0)
-                            map_y.Set(Of Single)(j, i, 0)
-                        End If
+                    Case 0 ' leave the original unmapped!
                     Case 1
                         map_x.Set(Of Single)(j, i, i)
                         map_y.Set(Of Single)(j, i, src.Rows - j)
@@ -34,9 +30,9 @@ Public Class Remap_Basics : Implements IDisposable
             Next
         Next
 
-        cv.Cv2.Remap(ocvb.color, ocvb.result1, map_x, map_y)
+        If direction <> 0 Then cv.Cv2.Remap(ocvb.color, ocvb.result1, map_x, map_y) Else ocvb.result1 = ocvb.color
 
-        If ocvb.frameCount Mod 10 = 0 Then
+        If ocvb.frameCount Mod 30 = 0 Then
             direction += 1
             direction = direction Mod 4
         End If
@@ -54,17 +50,20 @@ Public Class Remap_Flip : Implements IDisposable
         ocvb.desc = "Use flip to remap an image."
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
+        ocvb.label1 = Choose(direction + 1, "Remap_Flip - original", "Remap_Flip - flip horizontal", "Remap_Flip - flip veritical",
+                                            "Remap_Flip - flip horizontal and vertical")
+
         Select Case direction
-            Case 0 ' flip horizontally
+            Case 0 ' do nothing!
+                ocvb.color.CopyTo(ocvb.result1)
+            Case 1 ' flip vertically  
                 cv.Cv2.Flip(ocvb.color, ocvb.result1, cv.FlipMode.Y)
-            Case 1 ' flip vertically
+            Case 2 ' flip horizontally
                 cv.Cv2.Flip(ocvb.color, ocvb.result1, cv.FlipMode.X)
-            Case 2 ' flip horizontally and vertically
+            Case 3 ' flip horizontally and vertically
                 cv.Cv2.Flip(ocvb.color, ocvb.result1, cv.FlipMode.XY)
-            Case 3 ' do nothing!
-                ocvb.result1.CopyTo(ocvb.result1)
         End Select
-        If ocvb.frameCount Mod 10 = 0 Then
+        If ocvb.frameCount Mod 100 = 0 Then
             direction += 1
             direction = direction Mod 4
         End If
