cdee4e771900ff71d4c83b864ad77f2808e066b2
Bob Davies <bobdavies2000@gmail.com>
2020-03-24 17:15:38 -0700

    OpenCVSharp does not support converting grayscale Mat's to bitmap.  (Not implemented support for 'halftone')

--- VB_Classes/Bitmap.vb
+++ VB_Classes/Bitmap.vb
@@ -1,20 +1,19 @@
 ﻿Imports System.Drawing
 Imports cv = OpenCvSharp
 ' https://www.csharpcodi.com/csharp-examples/OpenCvSharp.Extensions.BitmapConverter.ToBitmap(OpenCvSharp.Mat)/
-'Public Class Bitmap_ToMat : Implements IDisposable
-'    Public Sub New(ocvb As AlgorithmData)
-'        ocvb.label1 = "Convert color bitmap to Mat"
-'        ocvb.label2 = "Convert gray bitmap to Mat"
-'        ocvb.desc = "Convert a color and grayscale bitmap to a cv.Mat"
-'    End Sub
-'    Public Sub Run(ocvb As AlgorithmData)
-'        Dim bitmap = New Bitmap(ocvb.parms.HomeDir + "Data/space_shuttle.jpg")
-'        ocvb.result1 = cv.Extensions.BitmapConverter.ToMat(bitmap)
+Public Class Bitmap_ToMat : Implements IDisposable
+    Public Sub New(ocvb As AlgorithmData)
+        ocvb.label1 = "Convert color bitmap to Mat"
+        ocvb.label2 = "Convert Mat to bitmap and then back to Mat"
+        ocvb.desc = "Convert a color and grayscale bitmap to a cv.Mat"
+    End Sub
+    Public Sub Run(ocvb As AlgorithmData)
+        Dim bitmap = New Bitmap(ocvb.parms.HomeDir + "Data/lena.jpg")
+        ocvb.result1 = cv.Extensions.BitmapConverter.ToMat(bitmap).Resize(ocvb.color.Size)
 
-'        Dim gray = ocvb.result1.CvtColor(cv.ColorConversionCodes.BGR2GRAY)
-'        bitmap = cv.Extensions.BitmapConverter.ToBitmap(gray)
-'        ocvb.result2 = cv.Extensions.BitmapConverter.ToMat(bitmap)
-'    End Sub
-'    Public Sub Dispose() Implements IDisposable.Dispose
-'    End Sub
-'End Class
+        bitmap = cv.Extensions.BitmapConverter.ToBitmap(ocvb.color)
+        ocvb.result2 = cv.Extensions.BitmapConverter.ToMat(bitmap)
+    End Sub
+    Public Sub Dispose() Implements IDisposable.Dispose
+    End Sub
+End Class
