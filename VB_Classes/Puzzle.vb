730844830611d03ad6beeba9fae6a464a45d6996
Bob Davies <bobdavies2000@gmail.com>
2020-04-25 14:03:54 -0700

    Clear improvements in puzzle solving but not completely there.

--- VB_Classes/Puzzle.vb
+++ VB_Classes/Puzzle.vb
@@ -2,6 +2,16 @@
 Imports System.Threading
 
 Module Puzzle_Solvers
+    Public Class fit
+        Public correlation As Single
+        Public index As Int32
+        Public neighbor As Int32
+        Sub New(corr As Single, i As Int32, n As Int32)
+            correlation = corr
+            index = i
+            neighbor = n
+        End Sub
+    End Class
     Public Class CompareCorrelations : Implements IComparer(Of Single)
         Public Function Compare(ByVal a As Single, ByVal b As Single) As Integer Implements IComparer(Of Single).Compare
             ' why have compare for just unequal?  So we can get duplicates.  Nothing below returns a zero (equal)
@@ -50,7 +60,7 @@ Public Class Puzzle_Basics : Implements IDisposable
             ' shuffle the roi's
             Dim scramIndex As Int32
             While inputROI.Count > 0
-                Dim index = ocvb.ms_rng.Next(0, inputROI.Count - 1)
+                Dim index = ocvb.ms_rng.Next(inputROI.Count - 1)
                 Dim roi = inputROI(index)
                 inputROI.RemoveAt(index) ' sampling without replacement
                 Dim roi2 = scrambled(scramIndex)
@@ -78,178 +88,266 @@ End Class
 
 
 
-Public Class Puzzle_SolverTopBottom : Implements IDisposable
+Public Class Puzzle_SolverBottomUp : Implements IDisposable
     Dim corr As MatchTemplate_Basics
+    Dim puzzle As Puzzle_Basics
     Public roilist() As cv.Rect
+    Public externalUse As Boolean
+    Dim check As New OptionsCheckbox
     Public Sub New(ocvb As AlgorithmData)
         corr = New MatchTemplate_Basics(ocvb)
         corr.externalUse = True
         corr.sliders.Visible = False
         corr.radio.Visible = False
 
-        ocvb.desc = "Put the puzzle back together."
+        puzzle = New Puzzle_Basics(ocvb)
+
+        check.Setup(ocvb, 1)
+        check.Box(0).Text = "Restart Puzzle"
+        check.Box(0).Checked = True
+        If ocvb.parms.ShowOptions Then check.Show()
+
+        ocvb.desc = "Put the puzzle back together using the correlation coefficients of the top and bottom of each ROI."
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
-        If roilist Is Nothing Then
-            ocvb.putText(New ActiveClass.TrueType("Currently this algorithm is only run in combination with Puzzle_Solver", 10, 50, RESULT2))
-            Exit Sub
+        Const correlationThreshold = 0.75
+        If externalUse = False Then
+            If ocvb.frameCount < 10 Then Exit Sub ' no startup dark images.
+            If check.Box(0).Checked Then
+                check.Box(0).Checked = False
+                puzzle.restartRequested = True
+                puzzle.Run(ocvb)
+                roilist = puzzle.grid.roiList.ToArray
+            End If
+        Else
+            check.Visible = False
+            puzzle.grid.sliders.Visible = False
         End If
 
-        Dim newHeight = CInt(ocvb.color.Width * ocvb.color.Height / roilist(0).Width)
-        Dim results As New cv.Mat(newHeight, roilist(0).Width, cv.MatType.CV_8UC3, 0)
-        Dim bestList As New SortedList(Of Single, cv.Point)(New CompareCorrelations)
+        Dim goodFits(roilist.Count - 1) As List(Of fit)
+        For i = 0 To goodFits.Count - 1
+            goodFits(i) = New List(Of fit)  ' loops are easier if we don't have to check for "nothing" entries
+        Next
         For i = 0 To roilist.Count - 1
             Dim roi1 = roilist(i)
-            Dim bestIndex = 0
-            Dim bestCorr As Single = -1
             For j = 0 To roilist.Count - 1
                 If i = j Then Continue For
                 Dim roi2 = roilist(j)
                 corr.sample1 = ocvb.result1(roi1).Row(roi1.Height - 1)
                 corr.sample2 = ocvb.result1(roi2).Row(0)
                 corr.Run(ocvb)
-                If bestCorr < corr.correlationMat.At(Of Single)(0, 0) Then
-                    bestIndex = j
-                    bestCorr = corr.correlationMat.At(Of Single)(0, 0)
+                If corr.correlationMat.At(Of Single)(0, 0) > 0 Then
+                    goodFits(i).Add(New fit(corr.correlationMat.At(Of Single)(0, 0), i, j))
                 End If
             Next
-            bestList.Add(bestCorr, New cv.Point(i, bestIndex))
         Next
 
-        Dim nextRoilist As New List(Of cv.Rect)
+        Dim botList As New List(Of fit)
+        Dim botTotal = CInt(ocvb.color.Width / roilist(0).Width)
+        Dim colTotal = CInt(ocvb.color.Height / roilist(0).Height)
+        Dim cutoff = correlationThreshold ' threshold for fit
+        While botList.Count < botTotal
+            botList.Clear()
+            For i = 0 To goodFits.Count - 1
+                Dim bot = -1
+                For j = 0 To goodFits(i).Count - 1
+                    Dim nextFit = goodFits(i).ElementAt(j)
+                    If nextFit.correlation > cutoff Then
+                        bot = i
+                        Exit For
+                    End If
+                Next
+                If bot = -1 Then
+                    Dim bestFit As New fit(-1, 0, 0)
+                    For j = 0 To goodFits(i).Count - 1
+                        Dim nextFit = goodFits(i).ElementAt(j)
+                        If bestFit.correlation < nextFit.correlation Then bestFit = nextFit
+                    Next
+                    botList.Add(bestFit)
+                End If
+            Next
+            cutoff += 0.01
+        End While
+
         Dim nextX As Integer
         Dim nextY As Integer
-        Dim bestArray = bestList.ToArray
-        For i = 0 To bestList.Count - 1
-            Dim roi1 = roilist(bestArray(i).Value.X)
-            Dim roi2 = roilist(bestArray(i).Value.Y)
-            If roi1.Width > 0 And roi2.Width > 0 Then
-                ocvb.result1(roi1).CopyTo(results(New cv.Rect(nextX, nextY, roi1.Width, roi1.Height)))
-                ocvb.result1(roi2).CopyTo(results(New cv.Rect(nextX, nextY + roi1.Height, roi2.Width, roi2.Height)))
-
-                nextRoilist.Add(New cv.Rect(nextX, nextY, roi2.Width, roi1.Height + roi2.Height))
-                nextY += roi1.Height + roi2.Height
-                roilist(bestArray(i).Value.X).Width = 0
-                roilist(bestArray(i).Value.Y).Width = 0
-            End If
+        For i = 0 To botList.Count - 1
+            Dim roi = roilist(botList(i).index)
+            ocvb.result1(roi).CopyTo(ocvb.result2(New cv.Rect(nextX, ocvb.color.Height - roi.Height, roi.Width, roi.Height)))
+            nextX += roi.Width
+            If nextX >= ocvb.color.Width Then Exit For
         Next
-        ' there may be some tiles that matched another neighbor slightly better.  Add the runt roi's here and match on the next round.
-        'For i = 0 To roilist.Count - 1
-        '    Dim roi = roilist(i)
-        '    If roi.Width > 0 Then
-        '        nextRoilist.Add(New cv.Rect(nextX, nextY, roi.Width, roi.Height))
-        '        ocvb.result1(roi).CopyTo(results(New cv.Rect(nextX, nextY, roi.Width, roi.Height)))
-        '        nextY += roi.Height
-        '        If nextY >= results.Height Then
-        '            nextX += roi.Width
-        '            nextY = 0
-        '        End If
-        '    End If
-        'Next
-        results.CopyTo(ocvb.result1)
-        roilist = nextRoilist.ToArray
+
+        Dim botindex = 0
+        Dim rectIndex = 0
+        For nextX = 0 To ocvb.color.Width - roilist(0).Width Step roilist(0).Width
+            rectIndex = botList(botindex).index
+            For nextY = ocvb.color.Height - roilist(0).Height * 2 To 0 Step -roilist(0).Height
+                Dim bestCorr As Single = -1
+                Dim bestI As Integer = 0
+                Dim bestJ As Integer = 0
+                For i = 0 To goodFits.Count - 1
+                    If i = rectIndex Then Continue For
+                    For j = 0 To goodFits(i).Count - 1
+                        Dim nextFit = goodFits(i).ElementAt(j)
+                        If nextFit.neighbor = rectIndex Then
+                            If bestCorr < nextFit.correlation Then
+                                bestJ = j
+                                bestI = i
+                                bestCorr = nextFit.correlation
+                            End If
+                        End If
+                    Next
+                Next
+                Dim roi = roilist(bestI)
+                goodFits(bestI).ElementAt(bestJ).correlation = 0
+                ocvb.result1(roi).CopyTo(ocvb.result2(New cv.Rect(nextX, nextY, roi.Width, roi.Height)))
+                rectIndex = bestI
+                'cv.Cv2.ImShow("result2", ocvb.result2)
+                'cv.Cv2.WaitKey()
+            Next
+            botindex += 1
+        Next
         ocvb.label1 = "Current input to puzzle solver"
         ocvb.label2 = "Current output of puzzle solver"
     End Sub
     Public Sub Dispose() Implements IDisposable.Dispose
         corr.Dispose()
+        puzzle.Dispose()
+        check.Dispose()
     End Sub
 End Class
 
 
-
-
-
-
-
-
-
-Public Class Puzzle_SolverSides : Implements IDisposable
+Public Class Puzzle_SolverLeftRight : Implements IDisposable
     Dim corr As MatchTemplate_Basics
+    Dim puzzle As Puzzle_Basics
     Public roilist() As cv.Rect
+    Public externalUse As Boolean
+    Dim check As New OptionsCheckbox
     Public Sub New(ocvb As AlgorithmData)
         corr = New MatchTemplate_Basics(ocvb)
         corr.externalUse = True
         corr.sliders.Visible = False
         corr.radio.Visible = False
 
-        ocvb.desc = "Put the puzzle back together."
+        puzzle = New Puzzle_Basics(ocvb)
+
+        check.Setup(ocvb, 1)
+        check.Box(0).Text = "Restart Puzzle"
+        check.Box(0).Checked = True
+        If ocvb.parms.ShowOptions Then check.Show()
+
+        ocvb.desc = "Put the puzzle back together using correlation coefficients of the sides of each ROI."
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
-        If roilist Is Nothing Then
-            ocvb.putText(New ActiveClass.TrueType("Currently this algorithm is only run in combination with Puzzle_Solver", 10, 50, RESULT2))
-            Exit Sub
+        Const correlationThreshold = 0.75
+        If externalUse = False Then
+            If ocvb.frameCount < 10 Then Exit Sub ' no startup dark images.
+            If check.Box(0).Checked Then
+                check.Box(0).Checked = False
+                puzzle.restartRequested = True
+                puzzle.Run(ocvb)
+                roilist = puzzle.grid.roiList.ToArray
+            End If
+        Else
+            check.Visible = False
+            puzzle.grid.sliders.Visible = False
         End If
-        Dim newWidth = CInt(ocvb.color.Width * ocvb.color.Height / roilist(0).Height)
-        Dim results As New cv.Mat(roilist(0).Height, newWidth, cv.MatType.CV_8UC3, 0)
-        Dim bestList As New SortedList(Of Single, cv.Point)(New CompareCorrelations)
+
+        Dim goodFits(roilist.Count - 1) As List(Of fit)
+        For i = 0 To goodFits.Count - 1
+            goodFits(i) = New List(Of fit)  ' loops are easier if we don't have to check for "nothing" entries
+        Next
         For i = 0 To roilist.Count - 1
             Dim roi1 = roilist(i)
-            Dim bestIndex = 0
-            Dim bestCorr As Single = -1
             For j = 0 To roilist.Count - 1
                 If i = j Then Continue For
                 Dim roi2 = roilist(j)
                 corr.sample1 = ocvb.result1(roi1).Col(roi1.Width - 1)
                 corr.sample2 = ocvb.result1(roi2).Col(0)
                 corr.Run(ocvb)
-                If bestCorr < corr.correlationMat.At(Of Single)(0, 0) Then
-                    bestIndex = j
-                    bestCorr = corr.correlationMat.At(Of Single)(0, 0)
+                If corr.correlationMat.At(Of Single)(0, 0) > 0 Then
+                    goodFits(i).Add(New fit(corr.correlationMat.At(Of Single)(0, 0), i, j))
                 End If
             Next
-            bestList.Add(bestCorr, New cv.Point(i, bestIndex))
         Next
 
-        Dim nextRoilist As New List(Of cv.Rect)
+        Dim rightList As New List(Of fit)
+        Dim rightTotal = CInt(ocvb.color.Width / roilist(0).Width)
+        Dim colTotal = CInt(ocvb.color.Height / roilist(0).Height)
+        Dim cutoff = correlationThreshold ' threshold for fit
+        While rightList.Count < rightTotal
+            rightList.Clear()
+            For i = 0 To goodFits.Count - 1
+                Dim right = -1
+                For j = 0 To goodFits(i).Count - 1
+                    Dim nextFit = goodFits(i).ElementAt(j)
+                    If nextFit.correlation > cutoff Then
+                        right = i
+                        Exit For
+                    End If
+                Next
+                If right = -1 Then
+                    Dim bestFit As New fit(-1, 0, 0)
+                    For j = 0 To goodFits(i).Count - 1
+                        Dim nextFit = goodFits(i).ElementAt(j)
+                        If bestFit.correlation < nextFit.correlation Then bestFit = nextFit
+                    Next
+                    rightList.Add(bestFit)
+                End If
+            Next
+            cutoff += 0.01
+        End While
+
         Dim nextX As Integer
         Dim nextY As Integer
-        Dim bestArray = bestList.ToArray
-
-        For i = 0 To bestList.Count - 1
-            Dim roi1 = roilist(bestArray(i).Value.X)
-            Dim roi2 = roilist(bestArray(i).Value.Y)
-            If roi1.Width > 0 And roi2.Width > 0 Then
-                ocvb.result1(roi1).CopyTo(results(New cv.Rect(nextX, nextY, roi1.Width, roi1.Height)))
-                ocvb.result1(roi2).CopyTo(results(New cv.Rect(nextX + roi1.Width, nextY, roi2.Width, roi2.Height)))
-
-                cv.Cv2.ImShow("results", results)
-                cv.Cv2.WaitKey()
-
-                nextRoilist.Add(New cv.Rect(nextX, nextY, roi1.Width + roi2.Width, roi1.Height))
-                nextX += roi1.Width + roi2.Width
-                roilist(bestArray(i).Value.X).Width = 0
-                roilist(bestArray(i).Value.Y).Width = 0
-            End If
+        For i = 0 To rightList.Count - 1
+            Dim roi = roilist(rightList(i).index)
+            ocvb.result1(roi).CopyTo(ocvb.result2(New cv.Rect(nextX, ocvb.color.Height - roi.Height, roi.Width, roi.Height)))
+            nextX += roi.Width
+            If nextX >= ocvb.color.Width Then Exit For
         Next
-        ' there may be some tiles that matched another neighbor slightly better.  Add the runt roi's here and match on the next round.
-        For i = 0 To roilist.Count - 1
-            Dim roi = roilist(i)
-            If roi.Width > 0 Then
-                nextRoilist.Add(New cv.Rect(nextX, nextY, roi.Width, roi.Height))
-                ocvb.result1(roi).CopyTo(results(New cv.Rect(nextX, nextY, roi.Width, roi.Height)))
-                nextY += roi.Height
-                If nextY >= results.Height Then
-                    nextX += roi.Width
-                    nextY = 0
-                End If
-            End If
+
+        Dim rightindex = 0
+        Dim rectIndex = 0
+        For nextY = 0 To ocvb.color.Height - roilist(0).Height Step roilist(0).Height
+            rectIndex = rightList(rightindex).index
+            For nextX = ocvb.color.Width - roilist(0).Width * 2 To 0 Step -roilist(0).Width
+                Dim bestCorr As Single = -1
+                Dim bestI As Integer = 0
+                Dim bestJ As Integer = 0
+                For i = 0 To goodFits.Count - 1
+                    If i = rectIndex Then Continue For
+                    For j = 0 To goodFits(i).Count - 1
+                        Dim nextFit = goodFits(i).ElementAt(j)
+                        If nextFit.neighbor = rectIndex Then
+                            If bestCorr < nextFit.correlation Then
+                                bestJ = j
+                                bestI = i
+                                bestCorr = nextFit.correlation
+                            End If
+                        End If
+                    Next
+                Next
+                Dim roi = roilist(bestI)
+                goodFits(bestI).ElementAt(bestJ).correlation = 0
+                ocvb.result1(roi).CopyTo(ocvb.result2(New cv.Rect(nextX, nextY, roi.Width, roi.Height)))
+                rectIndex = bestI
+                'cv.Cv2.ImShow("result2", ocvb.result2)
+                'cv.Cv2.WaitKey()
+            Next
+            rightindex += 1
         Next
-        results.CopyTo(ocvb.result1)
-        roilist = nextRoilist.ToArray
         ocvb.label1 = "Current input to puzzle solver"
         ocvb.label2 = "Current output of puzzle solver"
     End Sub
     Public Sub Dispose() Implements IDisposable.Dispose
         corr.Dispose()
+        puzzle.Dispose()
+        check.Dispose()
     End Sub
 End Class
-
-
-
-
-
-
 
 
 Public Class Puzzle_Solver : Implements IDisposable
@@ -272,24 +370,31 @@ Public Class Puzzle_Solver : Implements IDisposable
         ocvb.desc = "Put the puzzle back together."
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
+        Static initialList() As cv.Rect
         If ocvb.frameCount < 10 Then Exit Sub ' no startup dark images.
         If check.Box(0).Checked Then
             check.Box(0).Checked = False
             puzzle.Run(ocvb)
+            initialList = puzzle.grid.roiList.ToArray
         End If
-        topBot.roilist = puzzle.grid.roiList.ToArray
-        Static initialized As Boolean
-        If initialized = False Then
+        If initialList.Count > 1 Then
+            topBot.roilist = initialList
             topBot.Run(ocvb)
-            sides.roilist = topBot.roilist
-            sides.Run(ocvb)
-            initialized = True
+
+            If topBot.roilist.Count > 1 Then
+                sides.roilist = topBot.roilist
+                sides.Run(ocvb)
+            End If
         End If
+        initialList = sides.roilist ' for the next iteration
 
         ocvb.label1 = "Current input to puzzle solver"
         ocvb.label2 = "Current output of puzzle solver"
     End Sub
     Public Sub Dispose() Implements IDisposable.Dispose
         check.Dispose()
+        puzzle.Dispose()
+        sides.Dispose()
+        topBot.Dispose()
     End Sub
 End Class
