079e65704e023df495138154895cd2aebddd82d1
Bob Davies <bobdavies2000@gmail.com>
2020-06-10 17:28:00 -0700

    Threshold laplacian was failing - simple fix - but needs more work to understand what is being done...

--- VB_Classes/Threshold.vb
+++ VB_Classes/Threshold.vb
@@ -1,5 +1,4 @@
 Imports cv = OpenCvSharp
-
 Public Class Threshold_LaplacianFilter
     Inherits ocvbClass
     Dim edges As Filter_Laplacian
@@ -10,7 +9,7 @@ Public Class Threshold_LaplacianFilter
         edges = New Filter_Laplacian(ocvb)
         sliders.setupTrackBar1(ocvb, caller, "dist Threshold", 1, 100, 40)
         label1 = "Foreground Input"
-        ocvb.desc = "Threshold the output of a Laplacian derivative, mask with depth foreground."
+        ocvb.desc = "Threshold the output of a Laplacian derivative, mask with depth foreground.  needs more work"
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
         edges.src = src
@@ -18,16 +17,10 @@ Public Class Threshold_LaplacianFilter
         dst2 = edges.dst2
         trim.src = src
         trim.Run(ocvb)
-        Dim gray = trim.dst1
+        dst1 = trim.dst1
 
-        Dim mask = gray.Threshold(1, 255, cv.ThresholdTypes.BinaryInv)
-        gray.SetTo(0, mask)
-
-        Dim dist = gray.DistanceTransform(cv.DistanceTypes.L2, 3)
-        Dim dist32f = dist.Normalize(0, 1, cv.NormTypes.MinMax)
-        dist32f = dist.Threshold(sliders.TrackBar1.Value / 100, 1.0, cv.ThresholdTypes.Binary)
-
-        dist32f.ConvertTo(dst1, cv.MatType.CV_8UC1, 255)
+        Dim mask = dst1.Threshold(1, 255, cv.ThresholdTypes.BinaryInv).ConvertScaleAbs(255)
+        dst2.SetTo(0, mask)
     End Sub
 End Class
 
