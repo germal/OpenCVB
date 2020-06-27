f47cef8c4d0050f9a7e8fa871124d5c847281c69
Bob Davies <bobdavies2000@gmail.com>
2020-06-27 08:47:38 -0700

    Added an audio algorithm to read any format sound file and convert it to PCM.

--- VB_Classes/Sound.vb
+++ VB_Classes/Sound.vb
@@ -1,12 +1,22 @@
 ﻿Imports cv = OpenCvSharp
+Imports NAudio.Wave
 Public Class Sound_ToPCM
     Inherits ocvbClass
     Dim audio As New OptionsAudio
+    Dim reader As MediaFoundationReader
     Public Sub New(ocvb As AlgorithmData)
         setCaller(ocvb)
-        If ocvb.parms.ShowOptions Then audio.Show()
+        audio.Show()
+        audio.NewAudio(ocvb)
+        If audio.fileinfo.Exists Then
+            reader = New MediaFoundationReader(audio.fileinfo.FullName)
+            WaveFileWriter.CreateWaveFile("c:\temp\test.wav", reader)
+        End If
         ocvb.desc = "Load a .wav or .MP3 file and convert to PCM"
     End Sub
     Public Sub Run(ocvb As AlgorithmData)
+        If audio.fileinfo.Exists Then
+
+        End If
     End Sub
 End Class
