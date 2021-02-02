02b9951bd28d3a49b46f1b8cc156b09ba7ae9e02
Bob Davies <bobdavies2000@gmail.com>
2021-02-01 19:19:39 -0800

    Some experiments with FloodFill and MatchTemplate using MFD as input.

--- VB_Classes/MFD.vb
+++ VB_Classes/MFD.vb
@@ -3,8 +3,10 @@ Public Class MFD_Basics
     Inherits VBparent
     Public motion As Motion_Basics
     Public stableImg As cv.Mat
+    Dim dMax As Depth_SmoothMax
     Public Sub New()
         initParent()
+        dMax = New Depth_SmoothMax
         motion = New Motion_Basics
         If findfrm(caller + " Radio Options") Is Nothing Then
             radio.Setup(caller, 2)
@@ -17,6 +19,9 @@ Public Class MFD_Basics
     End Sub
     Public Sub Run()
         If task.intermediateReview = caller Then ocvb.intermediateObject = Me
+
+        dMax.src = src
+        dMax.Run()
 
         motion.src = task.color.CvtColor(cv.ColorConversionCodes.BGR2GRAY)
         motion.Run()
@@ -33,7 +38,6 @@ Public Class MFD_Basics
             stableImg = src.Clone
         Else
             For Each rect In motion.intersect.enclosingRects
-                dst2.Rectangle(rect, cv.Scalar.Yellow, 2)
                 If rect.Width And rect.Height Then src(rect).CopyTo(stableImg(rect))
             Next
         End If
@@ -107,6 +111,12 @@ Public Class MFD_Sobel
         initParent()
         mfd = New MFD_Basics
         sobel = New Edges_Sobel
+
+        If findfrm(caller + " Slider Options") Is Nothing Then
+            sliders.Setup(caller)
+            sliders.setupTrackBar(0, "Pixel threshold to zero", 0, 255, 100)
+        End If
+
         label1 = "Sobel edges of Motion-Filtered RGB"
         task.desc = "Stabilize the Sobel output with MFD"
     End Sub
@@ -118,9 +128,10 @@ Public Class MFD_Sobel
         dst2 = mfd.dst2
         label2 = mfd.label2
 
+        Static thresholdSlider = findSlider("Pixel threshold to zero")
         sobel.src = mfd.dst1
         sobel.Run()
-        dst1 = sobel.dst1
+        dst1 = sobel.dst1.Threshold(thresholdSlider.value, 0, cv.ThresholdTypes.Tozero)
     End Sub
 End Class
 
@@ -132,13 +143,11 @@ End Class
 
 Public Class MFD_BinarizedSobel
     Inherits VBparent
-    Dim mfd As MFD_Basics
-    Dim mfdNoDepth As MFD_Basics
+    Public mfd As MFD_Basics
     Dim sobel As Edges_BinarizedSobel
     Public Sub New()
         initParent()
         mfd = New MFD_Basics
-        mfdNoDepth = New MFD_Basics
         sobel = New Edges_BinarizedSobel
         label1 = "Binarized Sobel edges of Motion-Filtered RGB"
         task.desc = "Stabilize the binarized Sobel output with MFD"
@@ -149,15 +158,10 @@ Public Class MFD_BinarizedSobel
         mfd.src = src
         mfd.Run()
 
-        mfdNoDepth.src = task.inrange.nodepthMask
-        mfdNoDepth.Run()
-
         sobel.src = mfd.dst1.Clone
-        sobel.src.SetTo(0, mfdNoDepth.dst1)
         sobel.Run()
 
         dst1 = sobel.dst1
-        dst1.SetTo(0, mfdNoDepth.dst1)
         dst2 = sobel.dst2
     End Sub
 End Class
@@ -191,7 +195,6 @@ Public Class MFD_FloodFill
             sliders.Setup(caller)
             sliders.setupTrackBar(0, "FloodFill Step Size", 1, src.Cols / 2, 15)
             sliders.setupTrackBar(1, "FloodFill point distance from edge", 1, 25, 10)
-            sliders.setupTrackBar(2, "Minimum length for missing contours", 3, 25, 4)
         End If
 
         Dim paletteSlider = findSlider("Number of color transitions (Used only with Random)")
@@ -205,16 +208,32 @@ Public Class MFD_FloodFill
         Dim fill = fillSlider.value
         Dim stepSize = stepSlider.Value
 
+        Dim input = src.Clone
+        If input.Type <> cv.MatType.CV_8UC1 Then
+            sobel.src = src
+            sobel.Run()
+            input = sobel.dst2.Clone
+        End If
+
         Static MFD_OnOffRadio = findRadio("Use motion-filtered pixel values")
         Static saveStepSize As Integer
         Static saveFillDistance As Integer
         Static saveMFD_OnOff = MFD_OnOffRadio.checked
         Dim resetColors As Boolean
+        Static imageRects As New List(Of cv.Rect)
         If saveStepSize <> stepSize Or saveFillDistance <> fill Or MFD_OnOffRadio.checked <> saveMFD_OnOff Then
             resetColors = True
             saveStepSize = stepSize
             saveFillDistance = fill
             saveMFD_OnOff = MFD_OnOffRadio.checked
+            Dim inputRect As New cv.Rect(0, 0, fill, fill)
+            For y = fill To input.Height - fill - 1 Step stepSize
+                For x = fill To input.Width - fill - 1 Step stepSize
+                    inputRect.X = x
+                    inputRect.Y = y
+                    imageRects.Add(inputRect)
+                Next
+            Next
         End If
 
         If task.mouseClickFlag And task.mousePicTag = RESULT1 Then setMyActiveMat()
@@ -224,54 +243,35 @@ Public Class MFD_FloodFill
         rects.Clear()
         centroids.Clear()
 
-        Dim input = src.Clone
-        If input.Type <> cv.MatType.CV_8UC1 Then
-            sobel.src = src
-            sobel.Run()
-            input = sobel.dst2.Clone
-        End If
-
         Static zero As New cv.Scalar(0)
         Static maskRect = New cv.Rect(1, 1, input.Width, input.Height)
 
         Dim maskPlus = New cv.Mat(New cv.Size(input.Width + 2, input.Height + 2), cv.MatType.CV_8UC1, 0)
-        Dim rect As cv.Rect
-        Dim pt As cv.Point
-        Dim testCount As Integer
-        Dim floodCount As Integer
         floodPoints.Clear()
 
-        Dim inputRect As New cv.Rect(0, 0, fill, fill)
         Dim depthThreshold = fill * fill / 2
         Static lastFrame = input.Clone
         dst1 = input.Clone
-        For y = fill To input.Height - fill - 1 Step stepSize
-            For x = fill To input.Width - fill - 1 Step stepSize
-                testCount += 1
-                inputRect.X = x
-                inputRect.Y = y
-                Dim edgeCount = input(inputRect).CountNonZero
-                Dim depthCount = task.depth32f(inputRect).CountNonZero
-                If edgeCount = 0 And depthCount > depthThreshold Then
-                    floodCount += 1
-                    pt.X = x + fill / 2
-                    pt.Y = y + fill / 2
-                    Dim colorIndex = lastFrame.Get(Of Byte)(pt.Y, pt.X)
-                    If resetColors Or colorIndex = 0 Then colorIndex = (255 - masks.Count - 1) Mod 255
-                    Dim pixelCount = cv.Cv2.FloodFill(dst1, maskPlus, pt, cv.Scalar.All(colorIndex), rect, zero, zero, floodFlag Or (255 << 8))
+        For Each rect In imageRects
+            Dim edgeCount = input(rect).CountNonZero
+            Dim depthCount = task.depth32f(rect).CountNonZero
+            If edgeCount = 0 And depthCount > depthThreshold Then
+                Dim pt = New cv.Point(CInt(rect.X + fill / 2), CInt(rect.Y + fill / 2))
+                Dim colorIndex = lastFrame.Get(Of Byte)(pt.Y, pt.X)
+                If resetColors Or colorIndex = 0 Then colorIndex = (255 - masks.Count - 1) Mod 255
 
-                    If rect.Width And rect.Height Then
-                        floodPoints.Add(pt)
-                        Dim m = cv.Cv2.Moments(maskPlus(rect), True)
-                        Dim centroid = New cv.Point2f(rect.X + m.M10 / m.M00, rect.Y + m.M01 / m.M00)
-
-                        maskSizes.Add(pixelCount, masks.Count)
-                        masks.Add(maskPlus(maskRect)(rect))
-                        rects.Add(rect)
-                        centroids.Add(centroid)
-                    End If
+                Dim floodRect As cv.Rect
+                Dim pixelCount = cv.Cv2.FloodFill(dst1, maskPlus, pt, cv.Scalar.All(colorIndex), floodRect, zero, zero, floodFlag Or (255 << 8))
+                If floodRect.Width And floodRect.Height Then
+                    floodPoints.Add(pt)
+                    Dim m = cv.Cv2.Moments(maskPlus(rect), True)
+                    Dim centroid = New cv.Point2f(rect.X + m.M10 / m.M00, rect.Y + m.M01 / m.M00)
+                    maskSizes.Add(pixelCount, masks.Count)
+                    masks.Add(maskPlus(maskRect)(rect))
+                    rects.Add(rect)
+                    centroids.Add(centroid)
                 End If
-            Next
+            End If
         Next
 
         lastFrame = dst1.Clone
@@ -283,5 +283,43 @@ Public Class MFD_FloodFill
         For Each pt In floodPoints
             dst2.Circle(pt, ocvb.dotSize, cv.Scalar.Yellow, -1, cv.LineTypes.AntiAlias)
         Next
+
+        For Each rect In sobel.mfd.motion.intersect.enclosingRects
+            dst2.Rectangle(rect, cv.Scalar.Yellow, 1)
+        Next
+        label2 = sobel.mfd.label2
+
+    End Sub
+End Class
+
+
+
+
+
+
+
+
+
+Public Class MFD_MatchTemplate
+    Inherits VBparent
+    Dim match As MatchTemplate_Movement
+    Dim sobel As MFD_Sobel
+    Public Sub New()
+        initParent()
+        match = New MatchTemplate_Movement
+        sobel = New MFD_Sobel
+        task.desc = "Use MFD_Sobel input to the MatchTemplate algorithm"
+    End Sub
+    Public Sub Run()
+        If task.intermediateReview = caller Then ocvb.intermediateObject = Me
+
+        sobel.src = src
+        sobel.Run()
+
+        match.src = sobel.dst1
+        match.Run()
+        dst1 = match.dst2
+        label1 = match.label2
+        dst2.SetTo(0)
     End Sub
 End Class
