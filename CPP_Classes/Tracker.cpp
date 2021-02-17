d098745102f02bea16b43b9365a27b6cb7198c96
Bob Davies <bobdavies2000@gmail.com>
2021-02-17 12:47:08 -0800

    lsd library still had some references to the original download area.  Also, reconfigured CPP_classes to use lib/Release/lsd.lib as input.  Started a C++ version of the Tracker_Basics because the OpenCVSharp interface is radically changed and not working properly.

---
+++ CPP_Classes/Tracker.cpp
@@ -0,0 +1,85 @@
+// Cut and paste this code to a module in the "CPP_Classes" project for the C++ interface.
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+
+
+using namespace std;
+using namespace cv;
+class Tracker_Basics
+{
+private:
+public:
+    Ptr<Tracker> tracker;
+    bool bboxInitialized = false;
+    Mat src, dst;
+    Tracker_Basics(int trackType)
+    {
+        // switch is based on the older tracking alternatives.  Some are disabled in the user interface for now.
+        switch (trackType)
+        {
+        case 0: // Boosting
+            break;
+        case 1: // MIL
+            tracker = cv::TrackerMIL::create();
+            break;
+        case 2: // KCF
+            tracker = cv::TrackerKCF::create();
+            break;
+        case 3: // TLD
+            break;
+        case 4: // MEDIANFLOW
+            break;
+        case 5: // GOTURN
+            tracker = cv::TrackerGOTURN::create();
+            break;
+        case 6: // CSRT
+            tracker = cv::TrackerCSRT::create();
+            break;
+        }
+    }
+    void Run(Rect bbox) {
+        if (bboxInitialized == false)
+        {
+            bboxInitialized = true;
+            tracker->init(src, bbox);
+        }
+        else {
+            bool ok = tracker->update(src, bbox);
+            if (ok)
+            {
+                rectangle(src, bbox, Scalar(255, 255, 255), 2, 1);
+            } else {
+                putText(src, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
+                bboxInitialized = false;
+            }
+        }
+    }
+};
+
+extern "C" __declspec(dllexport)
+Tracker_Basics *Tracker_Basics_Open(int trackType) {
+    Tracker_Basics *cPtr = new Tracker_Basics(trackType);
+    return cPtr;
+}
+
+extern "C" __declspec(dllexport)
+void Tracker_Basics_Close(Tracker_Basics *cPtr)
+{
+    delete cPtr;
+}
+
+extern "C" __declspec(dllexport)
+int *Tracker_Basics_Run(Tracker_Basics *cPtr, int *rgbPtr, int rows, int cols, int channels, int trackType, int x, int y, int w, int h)
+{
+		cPtr->src = Mat(rows, cols, (channels == 3) ? CV_8UC3 : CV_8UC1, rgbPtr);
+        Rect bbox(x, y, w, h);
+		cPtr->Run(bbox);
+		return (int *) cPtr->dst.data; // return this C++ allocated data to managed code
+}
--- VB_Classes/Tracker.vb
+++ VB_Classes/Tracker.vb
@@ -1,59 +1,81 @@
 Imports cv = OpenCvSharp
+Imports System.Runtime.InteropServices
 'Public Class Tracker_Basics
 '    Inherits VBparent
-'    Public tracker As cv.Tracking.MultiTracker
+'    Dim tracker As Object
+'    Public trackerCSRT As cv.Tracking.TrackerCSRT
+'    Public trackerGOTURN As cv.TrackerGOTURN
+'    Public trackerMIL As cv.TrackerMIL
+'    Public trackerKCF As cv.Tracking.TrackerKCF
 '    Public bbox As cv.Rect2d
 '    Public boxObject() As cv.Rect2d
-'    Public trackerIndex As integer = 5 ' trackerMIL by default...
+'    Public trackerIndex As Integer
 '    Public Sub New()
 '        initParent()
+
+'        If findfrm(caller + " Radio Options") Is Nothing Then
+'            radio.Setup(caller, 8)
+'            radio.check(0).Text = "TrackerCSRT - Channel and Spatial Reliability Tracker"
+'            radio.check(1).Text = "TrackerGOTURN"
+'            radio.check(2).Text = "TrackerMIL"
+'            radio.check(3).Text = "TrackerKCF"
+'            radio.check(0).Checked = True ' TrackerMIL is the default
+'        End If
+
 '        If findfrm(caller + " CheckBox Options") Is Nothing Then
 '            check.Setup(caller, 1)
 '            check.Box(0).Text = "Stop tracking selected object"
 '        End If
-'        task.desc = "Track an object using cv.Tracking API - tracker algorithm"
+
+'        task.desc = "Track an object using cv.Tracking API's - tracker algorithm"
 '    End Sub
 '    Public Sub Run()
-'		If task.intermediateReview = caller Then ocvb.intermediateObject = Me
-'        ocvb.trueText("Draw a rectangle around object to be tracked.", 10, 140)
+'        If task.intermediateReview = caller Then ocvb.intermediateObject = Me
+
+'        Dim input = src
+'        If input.Channels <> 1 Then input = input.CvtColor(cv.ColorConversionCodes.BGR2GRAY)
+
+'        ocvb.trueText("Draw a rectangle around object to be tracked.", 10, 140, 3)
 '        If check.Box(0).Checked Then
 '            check.Box(0).Checked = False
 '            If tracker IsNot Nothing Then tracker.Dispose()
 '            tracker = Nothing
 '        End If
+
+'        Static frm = findfrm(caller + " Radio Options")
+'        For i = 0 To frm.check.length - 1
+'            If frm.check(i).Checked = True Then
+'                label1 = "Method: " + radio.check(i).Text
+'                trackerIndex = i
+'                Exit For
+'            End If
+'        Next
+
 '        If task.drawRect.Width <> 0 Then
-'            tracker = cv.Tracking.MultiTracker.Create()
-'            Dim r = task.drawRect
-'            bbox = New cv.Rect2d(r.X, r.Y, r.Width, r.Height) ' silly that this isn't the same as rect.
-'            task.drawRectClear = True
+'            Dim mask = New cv.Mat(dst1.Size, cv.MatType.CV_8U, 0)
+'            mask.Rectangle(task.drawRect, cv.Scalar.White, -1)
 '            Select Case trackerIndex
 '                Case 0
-'                    tracker.Add(cv.Tracking.TrackerBoosting.Create(), src, bbox)
-'                Case 1
-'                    tracker.Add(cv.Tracking.TrackerCSRT.Create(), src, bbox)
-'                Case 2
-'                    tracker.Add(cv.Tracking.TrackerGOTURN.Create(), src, bbox)
-'                Case 3
-'                    tracker.Add(cv.Tracking.TrackerKCF.Create(), src, bbox)
-'                Case 4
-'                    tracker.Add(cv.Tracking.TrackerMedianFlow.Create(), src, bbox)
-'                Case 5
-'                    tracker.Add(cv.Tracking.TrackerMIL.Create(), src, bbox)
-'                Case 6
-'                    tracker.Add(cv.Tracking.TrackerMOSSE.Create(), src, bbox)
-'                Case 7
-'                    tracker.Add(cv.Tracking.TrackerTLD.Create(), src, bbox)
+'                    trackerCSRT = cv.Tracking.TrackerCSRT.Create()
+'                    trackerCSRT.Init(input, task.drawRect)
+'                    trackerCSRT.SetInitialMask(mask)
+
+'                    tracker = trackerCSRT
+'                    'Case 1
+'                    '    trackerGOTURN.Add(cv.TrackerGOTURN.Create, src, bbox)
+'                    'Case 2
+'                    '    trackerMIL.Add(cv.TrackerMIL.Create, src, bbox)
+'                    'Case 3
+'                    '    tracker.Add(cv.Tracking.TrackerKCF.Create, src, bbox)
 '            End Select
 '        End If
 
-'        dst2 = src.Clone()
+'        dst1 = input.Clone()
 '        If tracker IsNot Nothing Then
-'            tracker.Update(src)
-'            boxObject = tracker.GetObjects() ' just track one.  Tracking multiple is buggy.  Returns a lot of 0 width/height rect2d's.
-'            Dim p1 = New cv.Point(boxObject(0).X, boxObject(0).Y)
-'            Dim p2 = New cv.Point(boxObject(0).X + bbox.Width, boxObject(0).Y + bbox.Height)
-'            dst2.Rectangle(p1, p2, cv.Scalar.Blue, 2)
+'            trackerCSRT.Update(input, task.drawRect)
+'            dst1.Rectangle(task.drawRect, cv.Scalar.Yellow, 2)
 '        End If
+
 '    End Sub
 'End Class
 
@@ -69,7 +91,7 @@ Imports cv = OpenCvSharp
 '        task.desc = "Track any number of objects simultaneously - tracker algorithm"
 '    End Sub
 '    Public Sub Run()
-'		If task.intermediateReview = caller Then ocvb.intermediateObject = Me
+'        If task.intermediateReview = caller Then ocvb.intermediateObject = Me
 '        If task.drawRect.Width <> 0 Then
 '            Dim tr = New Tracker_Basics()
 '            tr.src = src
@@ -98,48 +120,90 @@ Imports cv = OpenCvSharp
 
 
 
-'Public Class Tracker_Methods
-'    Inherits VBparent
-'    Public tracker As cv.Tracking.Tracker
-'    Public Sub New()
-'        initParent()
-
-'        If findfrm(caller + " Radio Options") Is Nothing Then
-'            radio.Setup(caller, 8)
-'            radio.check(1).Text = "TrackerCSRT"
-'            radio.check(2).Text = "TrackerGOTURN"
-'            radio.check(2).Enabled = False
-'            radio.check(3).Text = "TrackerKCF"
-'            radio.check(5).Text = "TrackerMIL"
-'            radio.check(6).Text = "TrackerMOSSE"
-'            radio.check(7).Text = "TrackerTLD"
-'            radio.check(5).Checked = True ' TrackerMIL is the default
-'        End If
-
-'        task.desc = "Experiment with the different types of tracking methods - apparently not much difference..."
-'    End Sub
-'    Public Sub Run()
-'        If task.intermediateReview = caller Then ocvb.intermediateObject = Me
-'        Static saveMethod As Integer
-
-'        Static frm = findfrm("Tracker_Methods Radio Options")
-'        For i = 0 To frm.check.length - 1
-'            If frm.check(i).Checked = True Then
-'                ' tracker.trackerIndex = i
-'                cv.Tracking.TrackerCSRT.Params()
-'                label1 = "Method: " + radio.check(i).Text
-'                Exit For
-'            End If
-'        Next
-'        If saveMethod <> tracker.trackerIndex Then
-'            tracker.check.Box(0).Checked = True
-'        Else
-'            tracker.src = src
-'            tracker.Run()
-'            dst1 = tracker.dst1
-'        End If
-'        saveMethod = tracker.trackerIndex
-'    End Sub
-'End Class
 
 
+
+Module Tracker_Basics_CPP_Module
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Function Tracker_Basics_Open(trackType As Integer) As IntPtr
+    End Function
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Sub Tracker_Basics_Close(cPtr As IntPtr)
+    End Sub
+    <DllImport(("CPP_Classes.dll"), CallingConvention:=CallingConvention.Cdecl)>
+    Public Function Tracker_Basics_Run(cPtr As IntPtr, rgbPtr As IntPtr, rows As Int32, cols As Int32, channels As Int32,
+                                       x As Integer, y As Integer, w As Integer, h As Integer) As IntPtr
+    End Function
+End Module
+
+
+
+Public Class Tracker_Basics
+    Inherits VBparent
+    Dim cPtr As IntPtr
+    Dim trackType As Integer
+    Public Sub New()
+        initParent()
+
+        If findfrm(caller + " Radio Options") Is Nothing Then
+            radio.Setup(caller, 8)
+            radio.check(0).Text = "Boosting"
+            radio.check(1).Text = "MIL"
+            radio.check(2).Text = "KCF"
+            radio.check(3).Text = "TLD"
+            radio.check(4).Text = "MedianFlow"
+            radio.check(5).Text = "GoTurn"
+            radio.check(6).Text = "Mosse"
+            radio.check(7).Text = "TrackerCSRT - Channel and Spatial Reliability Tracker"
+            radio.check(0).Checked = True ' TrackerMIL is the default
+        End If
+
+        task.desc = "Use C++ to track objects"
+    End Sub
+    Public Sub Run()
+        If task.intermediateReview = caller Then ocvb.intermediateObject = Me
+
+
+        Dim input = src
+        If input.Channels <> 1 Then input = input.CvtColor(cv.ColorConversionCodes.BGR2GRAY)
+
+        Static saveTrackerType = -1
+        If saveTrackerType <> trackType Then
+            Static frm = findfrm(caller + " Radio Options")
+            For i = 0 To frm.check.length - 1
+                If frm.check(i).Checked = True Then
+                    label1 = "Method: " + radio.check(i).Text
+                    trackType = i
+                    Exit For
+                End If
+            Next
+            If saveTrackerType < 0 Then
+                cPtr = Tracker_Basics_Open(trackType)
+            Else
+                Tracker_Basics_Close(cPtr)
+                cPtr = Tracker_Basics_Open(trackType)
+            End If
+            saveTrackerType = trackType
+        End If
+
+        If task.drawRect.Width <> 0 Then
+            Dim srcData(input.Total * input.ElemSize) As Byte
+            Marshal.Copy(input.Data, srcData, 0, srcData.Length - 1)
+            Dim handleSrc = GCHandle.Alloc(srcData, GCHandleType.Pinned)
+            Dim r = task.drawRect
+            Dim imagePtr = Tracker_Basics_Run(cPtr, handleSrc.AddrOfPinnedObject(), input.Rows, input.Cols, input.Channels, r.X, r.Y, r.Width, r.Height)
+            handleSrc.Free()
+
+            If imagePtr <> 0 Then
+                Dim dstData(input.Total * input.ElemSize - 1) As Byte
+                Marshal.Copy(imagePtr, dstData, 0, dstData.Length)
+                dst1 = New cv.Mat(input.Rows, input.Cols, If(input.Channels = 3, cv.MatType.CV_8UC3, cv.MatType.CV_8UC1), dstData)
+            End If
+        Else
+            ocvb.trueText("Draw a rectangle around any object to be tracked in the RGB image above.", 10, 140)
+        End If
+    End Sub
+    Public Sub Close()
+        Tracker_Basics_Close(cPtr)
+    End Sub
+End Class
